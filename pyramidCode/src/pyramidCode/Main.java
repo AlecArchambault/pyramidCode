36f60e2d731230eae6b0467c194997a77ee5bec7
AlecArchambault <aarchambault@wisc.edu>
2020-01-31 20:18:51 -0600

    no message

--- pyramidCode/src/pyramidCode/Main.java
+++ pyramidCode/src/pyramidCode/Main.java
@@ -1,14 +1,55 @@
 package pyramidCode;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.util.EntityUtils;
+
 public class Main {
-   //Rewrite so that it uses get on a url input and based on the boolean reponse returns the correct http response
-	public static void main(String[] args) {
-	   System.out.print("Enter string: ");
-	   Scanner input = new Scanner(System.in);
-	   String in = input.nextLine();
-	   System.out.print(test(in));
+   @SuppressWarnings("deprecation")
+public static void main(String[] args) throws Exception {
+	if(args.length == 0) {
+		return;
+	}
+	DefaultHttpClient httpClient = new DefaultHttpClient();
+	try
+	    {
+	        
+			HttpGet getRequest = new HttpGet(args[0]);
+	        getRequest.addHeader("accept", "application/xml");
+	        
+	        HttpResponse test = httpClient.execute(getRequest);
+	        HttpResponse response;
+	        //verify the valid error code first
+	        int statusCode = test.getStatusLine().getStatusCode();
+	        if (statusCode != 200) 
+	        {
+	            throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+	        }
+	         
+	        //Now pull back the response object
+	        HttpEntity httpEntity = test.getEntity();
+	        String pyramidWord = EntityUtils.toString(httpEntity);
+	        if(test(pyramidWord)) {
+	        	//If it is a pyramid word
+	        	response = new BasicHttpResponse(HttpVersion.HTTP_1_1, 
+	        			HttpStatus.SC_OK, "OK");
+	        } else {
+	        	//If it is not a pyramid word
+	        	response = new BasicHttpResponse(HttpVersion.HTTP_1_1, 
+	        			HttpStatus.SC_OK, "BAD REQUEST");
+	        }
+	    } finally{
+	    	httpClient.getConnectionManager().shutdown();
+	    }
    }
 	
 	public static boolean test(String args) {
